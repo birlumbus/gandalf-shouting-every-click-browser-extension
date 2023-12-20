a9ecbbebcb008efc951fedbaa229ebcd4b3f5a70
turtelneck <65428036+turtelneck@users.noreply.github.com>
2023-12-20 11:10:21 -0700

    Final attempt

--- content.js
+++ content.js
@@ -1,26 +1,54 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log("content.js: RuntimeListener: initialized")
+console.log("Initializing audio variables");
+const audioTestRawURL = new Audio('audio/pop.mp3');
+const audioTestGetURL = new Audio(chrome.runtime.getURL('/audio/pop.mp3'));
+console.log("Audio files created");
 
-  if (message.action === 'playClickSound') {
-    console.log("content.js: RuntimeListener: calling playSound()")
-    playSound();
-    console.log("content.js: RuntimeListener: playSound() called")
-  }
-});
+function preAudioPrint() {
+  console.log("Audio files initialized. Playing:");
+}
+
+function postAudioPrint() {
+  console.log("Audio should have played");
+}
+
+function audioTest1() {
+  audioTestRawURL.play();
+  console.log("playing audioTestRawURL");
+}
+
+function audioTest2() {
+  audioTestGetURL.play();
+  console.log("playing audioTestGetURL");
+}
+
+function endOfScript() {
+  console.log("End of script");
+}
+
+setTimeout(preAudioPrint, 500);
+setTimeout(audioTest1, 1500);
+setTimeout(audioTest2, 2500);
+setTimeout(postAudioPrint, 3500);
+setTimeout(endOfScript, 4000);
 
 document.addEventListener('click', () => {
-  console.log("content.js: EventListener: click detected")
+  console.log("Entering addEventListener()");
+  console.log(chrome.runtime.getURL('pop.mp3'));
+  console.log("tried getting url");
+  console.log("moving on now to the rest");
 
-  // Trigger the playClickSound function in background.js
-  chrome.runtime.sendMessage({ action: 'playClickSound' });
+  console.log("content.js: EventListener: click detected");
 
-  console.log("content.js: EventListener: message sent to runtime")
+  playSound();
+
+  console.log("content.js: EventListener: sound played");
 });
 
 function playSound() {
-  console.log("content.js: playSound(): entered function")
-  let audio = new Audio(chrome.runtime.getURL('audio/pop.mp3'));
-  console.log("content.js: playSound(): audio created")
+  console.log("content.js: playSound(): entered function");
+  let audio = new Audio(chrome.runtime.getURL('pop.mp3'));
+  console.log("content.js: playSound(): audio created");
   audio.play();
-  console.log("content.js: playSound(): audio.play() called")
+  console.log("content.js: playSound(): audio.play() called");
 }
+
